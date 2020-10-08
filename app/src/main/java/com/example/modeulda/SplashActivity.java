cb0583bffb4ab3f4d9b75c05edf25fdaca8ac312
ColaTasteKimchi <daniel.jaeha.lee@gmail.com>
2020-10-08 15:46:37 +0900

    스필래시 대충

--- app/src/main/java/com/example/modeulda/SplashActivity.java
+++ app/src/main/java/com/example/modeulda/SplashActivity.java
@@ -2,7 +2,10 @@ package com.example.modeulda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //로그인 창 만들고 난 뒤에  if문 넣어서 합시다.
+                SplashActivity.this.startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 1500);
     }
 }
