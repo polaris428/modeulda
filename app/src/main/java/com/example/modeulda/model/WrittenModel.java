854eafacf425254d50f663c34d2dd6f46d68a65b
ColaTasteKimchi <daniel.jaeha.lee@gmail.com>
2020-10-27 03:11:57 +0900

    글모델 만듬

--- app/src/main/java/com/example/modeulda/model/WrittenModel.java
+++ app/src/main/java/com/example/modeulda/model/WrittenModel.java
@@ -1,4 +1,54 @@
 package com.example.modeulda.model;
 
 public class WrittenModel {
+    public String title, author, content, time, likes;
+    public WrittenModel(){}
+    public WrittenModel(String title, String author, String content, String time, String likes){
+        this.title = title;
+        this.author = author;
+        this.content = content;
+        this.time = time;
+        this.likes = likes;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+    public void setLikes(String likes) {
+        this.likes = likes;
+    }
+
+
 }
