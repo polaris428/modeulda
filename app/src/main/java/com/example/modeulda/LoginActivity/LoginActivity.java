6c8cba2274b723b375dfd661d439ab5f125ac787
ColaTasteKimchi <daniel.jaeha.lee@gmail.com>
2020-10-26 21:53:35 +0900

    음! 로그인 만드는 중!

--- app/src/main/java/com/example/modeulda/LoginActivity/LoginActivity.java
+++ app/src/main/java/com/example/modeulda/LoginActivity/LoginActivity.java
@@ -1,16 +1,41 @@
 package com.example.modeulda.LoginActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 
 import com.example.modeulda.R;
+import com.example.modeulda.databinding.ActivityLoginBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class LoginActivity extends AppCompatActivity {
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    private ActivityLoginBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
+        binding.setEmail("");
+        binding.setPassword("");
+
+        binding.btnLoginSignin.setOnClickListener(view -> {
+            login(binding.getEmail(), binding.getPassword());
+        });
+
+    }
+
+    private void login(String email, String pw) {
+        if(email.isEmpty() || pw.isEmpty()){
+
+        }
     }
 }
