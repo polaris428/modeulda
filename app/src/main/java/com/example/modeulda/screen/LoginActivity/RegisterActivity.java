6a3267b07064fc7e9031b74f0fb1122bc6cf7305
ColaTasteKimchi <daniel.jaeha.lee@gmail.com>
2020-10-28 15:07:04 +0900

    회원가입창 xml 대충 완료 자바 시작 2020/10/28 이재하

--- app/src/main/java/com/example/modeulda/screen/LoginActivity/RegisterActivity.java
+++ app/src/main/java/com/example/modeulda/screen/LoginActivity/RegisterActivity.java
@@ -1,16 +1,35 @@
 package com.example.modeulda.screen.LoginActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
 import com.example.modeulda.R;
+import com.example.modeulda.databinding.ActivityRegisterBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity {
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+    private ActivityRegisterBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register);
+        binding.setEmail("");
+        binding.setId("");
+        binding.setPw1("");
+        binding.setPw2("");
+        binding.btnRegiSignup.setOnClickListener(view -> {
+            register(binding.getId(), binding.getEmail(), binding.getPw1(), binding.getPw2());
+        });
+    }
+
+    private void register(String id, String email, String pw1, String pw2) {
+        return;
     }
 }
