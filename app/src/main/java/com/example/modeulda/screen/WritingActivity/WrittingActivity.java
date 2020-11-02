b45e8a185fd1170ad2ed1e844cf49230005806ae
ColaTasteKimchi <daniel.jaeha.lee@gmail.com>
2020-11-02 11:33:52 +0900

    해결

---
+++ app/src/main/java/com/example/modeulda/screen/WritingActivity/WrittingActivity.java
@@ -0,0 +1,20 @@
+package com.example.modeulda.screen.WritingActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.modeulda.R;
+
+public class WrittingActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writting);
+
+    }
+}
