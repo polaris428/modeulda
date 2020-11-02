7c5c4728e3a7a47c0ed032310923709ceae881c9
ColaTasteKimchi <daniel.jaeha.lee@gmail.com>
2020-11-03 08:24:53 +0900

    2020/11/3
    하루의 시작은 푸쉬풀

--- app/src/main/java/com/example/modeulda/screen/MainActivity/Main2Fragment.java
+++ app/src/main/java/com/example/modeulda/screen/MainActivity/Main2Fragment.java
@@ -8,13 +8,18 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ObservableArrayList;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.example.modelda.screen.MainActivity.TodayAdapter;
 import com.example.modeulda.R;
 import com.example.modeulda.databinding.FragmentMain2Binding;
+import com.example.modeulda.model.WrittenModel;
 
 public class Main2Fragment extends Fragment {
 
+    private ObservableArrayList<WrittenModel> items = new ObservableArrayList<>();
     private FragmentMain2Binding binding;
     private Context mContext;
 
@@ -28,6 +33,14 @@ public class Main2Fragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main2, container, false);
 
+        binding.setItems(items);
+
+        binding.recMain2content.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
+        com.example.modelda.screen.MainActivity.TodayAdapter todayAdapter = new TodayAdapter();
+        binding.recMain2content.setAdapter(todayAdapter);
+        todayAdapter.setOnItemClickListener((view, item) -> {
+
+        });
         binding.txtMain2to1.setOnClickListener(view -> switchFinF());
         return binding.getRoot();
     }
@@ -35,4 +48,6 @@ public class Main2Fragment extends Fragment {
     private void switchFinF() {
         ((MainActivity) getActivity()).switchFragment(new Main1Fragment());
     }
+
+
 }
