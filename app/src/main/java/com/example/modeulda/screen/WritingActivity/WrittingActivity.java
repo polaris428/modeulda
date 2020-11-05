6e7739f025546beafc2ed3d7a2ef7424db30bd25
polaris428 <61411085+polaris428@users.noreply.github.com>
2020-11-06 07:23:50 +0900

    애니메이션 작업 1

--- app/src/main/java/com/example/modeulda/screen/WritingActivity/WrittingActivity.java
+++ app/src/main/java/com/example/modeulda/screen/WritingActivity/WrittingActivity.java
@@ -1,6 +1,11 @@
 package com.example.modeulda.screen.WritingActivity;
 
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,10 +19,25 @@ public class WrittingActivity extends AppCompatActivity {
    private ActivityWrittingBinding binding;
     private TextView mTextView;
     public WrittenModel model;
+    private Button btns;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_writting);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_writting);
+
+
+
+        Animation mAnim1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotae);
+        mAnim1.setInterpolator(getApplicationContext(), android.R.anim.accelerate_interpolator);
+
+        btns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                btns.startActionMode((ActionMode.Callback) mAnim1);
+            }
+        });
+
 
     }
 }
