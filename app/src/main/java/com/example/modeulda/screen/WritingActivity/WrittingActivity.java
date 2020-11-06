9f18c63378483a810cec28634b647a0b86a3511d
ColaTasteKimchi <daniel.jaeha.lee@gmail.com>
2020-11-06 22:38:50 +0900

    ^^

--- app/src/main/java/com/example/modeulda/screen/WritingActivity/WrittingActivity.java
+++ app/src/main/java/com/example/modeulda/screen/WritingActivity/WrittingActivity.java
@@ -1,5 +1,6 @@
 package com.example.modeulda.screen.WritingActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,31 +12,25 @@ import com.example.modeulda.ModelDoc.WrittenModel;
 import com.example.modeulda.R;
 import com.example.modeulda.databinding.ActivityWrittingBinding;
 
+import java.util.List;
+
 public class WrittingActivity extends AppCompatActivity {
    private ActivityWrittingBinding binding;
     private TextView mTextView;
     public WrittenModel model;
     private Button btns;
+    private Context mContext;
+   private List<String> list;
+    public int i =0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_writting);
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_writting);
-//        Animation mAnim1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotae);
-//        mAnim1.setInterpolator(getApplicationContext(), android.R.anim.accelerate_interpolator);
-//
-//        binding.btns.setOnClickListener(view -> {
-//            btns.startActionMode((ActionMode.Callback) mAnim1);
-//        });
-//
-//
-//        btns.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                btns.startActionMode((ActionMode.Callback) mAnim1);
-//            }
-//        });
 
 
+        list.add(binding.getContent());
     }
 }
