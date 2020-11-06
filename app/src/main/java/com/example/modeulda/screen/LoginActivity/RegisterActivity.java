eb8271b5f5a3d49adff5481b234d5d281e8d1057
polaris428 <61411085+polaris428@users.noreply.github.com>
2020-11-06 22:53:36 +0900

    sdaf

--- app/src/main/java/com/example/modeulda/screen/LoginActivity/RegisterActivity.java
+++ app/src/main/java/com/example/modeulda/screen/LoginActivity/RegisterActivity.java
@@ -37,30 +37,22 @@ public class RegisterActivity extends AppCompatActivity {
         binding.btnRegiSignup.setOnClickListener(view -> {
             register(binding.getId(), binding.getEmail(), binding.getPw1(), binding.getPw2());
         });
-<<<<<<< Updated upstream
         //이메일 본인인증 활성화
         binding.cboxRegi.setOnClickListener(view -> binding.setBtnen(binding.cboxRegi.isChecked()));
         //이메일 본인인증 메일 송신 버튼
         binding.btnRegicer.setOnClickListener(view -> {
-                Certification();
-=======
-        binding.cboxRegi.setOnClickListener(view -> {
-            if(binding.cboxRegi.isChecked()){
-                binding.setBtnen(true);
-            }
->>>>>>> Stashed changes
+            Certification();
         });
     }
 
     //토스트들은 나중에 꼭 수정할것
     private void register(String id, String email, String pw1, String pw2) {
         if (id.isEmpty() || email.isEmpty() || pw1.isEmpty() || pw2.isEmpty()) {
-            Toast.makeText(this, "빈칸을 전부 채워 주세여", Toast.LENGTH_SHORT).show();
-
+            binding.msg.setText("빈칸을 전부 채워주세요");
             return;
         }
         if (!pw1.equals(pw2)) {
-            Toast.makeText(this, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show();
+            binding.pswmsg.setText("비밀번호가 일치하지 않습니다");
             return;
         }
         firebaseFirestore
@@ -98,5 +90,5 @@ public class RegisterActivity extends AppCompatActivity {
         return new SimpleDateFormat("MM/dd/yyyy hh:mm:ss", Locale.ENGLISH).format(new Date());
     }
     //08/18/2018 07:22:16
-   // yyyy/MM/dd hh:mm aa
+    // yyyy/MM/dd hh:mm aa
 }
